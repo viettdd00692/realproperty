5535d86c5ade03b48a7e7e7f27d180b5a5daa883
Duc Viet Tran <viettdd00692@fpt.edu.vn>
2020-06-10 09:39:05 +0700

    update ignore auth page

--- src/main/java/com/example/realproperty/RealpropertyApplication.java
+++ src/main/java/com/example/realproperty/RealpropertyApplication.java
@@ -2,12 +2,20 @@ package com.example.realproperty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class RealpropertyApplication {
+public class RealpropertyApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(RealpropertyApplication.class, args);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/");
     }
 
 }
