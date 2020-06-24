899a19d42f5fb3215713db0ba51464f0dec9d549
Dinh Tuan Nghia <nghiadtd00712@fpt.edu.vn>
2020-06-24 09:26:41 +0700

    Update MainController.java

--- src/main/java/com/example/realproperty/controller/MainController.java
+++ src/main/java/com/example/realproperty/controller/MainController.java
@@ -44,4 +44,9 @@ public class MainController {
         return "client/houseforsell";
     }
 
+    @GetMapping("/contact")
+    public String contact() {
+        return "client/contact";
+    }
+
 }
